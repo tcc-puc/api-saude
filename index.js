07515da81c88ed0893d26fe80a6241f80090c144
Luan Fagundes <luan.fagundes@pebmed.com.br>
2021-01-05 22:30:09 -0300

    padronizacao api

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+const mocks = require('./mocks.json');
 const express = require("express");
 
-const app = express();
+const server = express();
 
-app.get("/", (req, res) => {
-  res.send("API saúde is running...");
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("API saúde is running.");
 });
 
-app.listen(3032);
+server.get("/prontuario/:cpf", (req, res) => {
+
+  const { cpf } = req.params;
+
+  if (cpf === "12345678901") {
+    return res.status(200).send(mocks)
+  } else {
+    return res.status(404).json({
+        "error": "search-0002",
+        "message": "Dados nāo encontrados, por favor tente novamente.",
+        "detail": "Parece que houve um erro com a sua busca",
+        "help": ""
+    })
+  }
+});
+
+server.listen(3032);
